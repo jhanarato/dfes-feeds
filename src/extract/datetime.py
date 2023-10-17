57b1c6e2f458471836ce0ca94cae438b8279e63c
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-17 08:58:04 +0800

    Import datetime contents.

--- src/extract/datetime.py
+++ src/extract/datetime.py
@@ -1,26 +1,27 @@
-import datetime
 import re
+from datetime import date, time, datetime
 
 
-def extract_date(text: str | None) -> datetime.date | None:
+def extract_date(text: str | None) -> date | None:
     if text is None:
         return None
 
     if m := re.search(r"\d{1,2} \w+ \d{4}", text):
         try:
-            return datetime.datetime.strptime(m.group(0), "%d %B %Y").date()
+            return datetime.strptime(m.group(0), "%d %B %Y").date()
         except ValueError:
             return None
     return None
 
 
-def extract_time(text: str | None) -> datetime.time | None:
+def extract_time(text: str | None) -> time | None:
     if text is None:
         return None
 
     if m := re.search(r"\d{2}:\d{2} [A|P]M", text):
         try:
-            return datetime.datetime.strptime(m.group(0), "%M:%H %p").time()
+            return datetime.strptime(m.group(0), "%M:%H %p").time()
         except ValueError:
             return None
     return None
+
