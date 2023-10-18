97c60acfcadc4fed12fe47de7b74ea4bc2f5356a
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-18 10:29:53 +0800

    Filename fixtures.

--- tests/test_feeds.py
+++ tests/test_feeds.py
@@ -1,35 +1,39 @@
 import pytest
 
 import dfes.feeds
-from dfes import bans
+
+
+@pytest.fixture
+def with_entries():
+    return "data/2023-01-03/message_TFB.rss"
+
+
+@pytest.fixture
+def without_entries():
+    return "data/2023-10-14/message_TFB.rss"
 
 
 @pytest.mark.parametrize(
     "index", [0, 1, 2, 3]
 )
-def test_get_existing_summaries(index):
-    feed_location = "data/2023-01-03/message_TFB.rss"
-    summary = dfes.feeds.get_summary(feed_location, index)
+def test_get_existing_summaries(with_entries, index):
+    summary = dfes.feeds.get_summary(with_entries, index)
     assert summary[:5] == "<div>"
 
 
 @pytest.mark.parametrize(
     "index", [-1, 4]
 )
-def test_non_existing_summaries(index):
-    feed_location = "data/2023-01-03/message_TFB.rss"
+def test_non_existing_summaries(with_entries, index):
     with pytest.raises(IndexError):
-        _ = dfes.feeds.get_summary(feed_location, index)
+        _ = dfes.feeds.get_summary(with_entries, index)
 
 
-def test_summary_index():
-    feed_location = "data/2023-01-03/message_TFB.rss"
-
-    soups = [bans.get_soup(feed_location, index) for index in range(3)]
-    dates = [bans.date_of_issue(soup) for soup in soups]
-    assert dates == sorted(dates, reverse=True)
-
-
-def test_rss_has_no_entries():
+def test_rss_has_no_entries(without_entries):
     with pytest.raises(IndexError):
-        _ = dfes.feeds.get_summary("data/2023-10-14/message_TFB.rss")
+        _ = dfes.feeds.get_summary(without_entries)
+
+
+def test_get_date_published():
+    # assert dfes.feeds.get_published
+    pass
