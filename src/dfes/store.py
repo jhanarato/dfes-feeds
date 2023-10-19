d0e6351ac79e3a0d1d76af02315bce1b0c03f6f7
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-19 13:25:14 +0800

    Created an in memory version of the feed store.

--- src/dfes/store.py
+++ src/dfes/store.py
@@ -1,6 +1,15 @@
-from dfes.bans import TotalFireBans
+from dfes.bans import TotalFireBans, total_fire_bans
 
 
 def file_name(bans: TotalFireBans) -> str:
     date_formatted = bans.issued.strftime("%Y_%m_%d_%H%M")
     return f"total_fire_bans_issued_{date_formatted}.rss"
+
+
+class InMemoryStore:
+    def __init__(self):
+        self._feeds = dict()
+
+    def add(self, feed: str) -> None:
+        bans = total_fire_bans(feed)
+        self._feeds[file_name(bans)] = feed
--- tests/test_store.py
+++ tests/test_store.py
@@ -3,7 +3,7 @@ from datetime import datetime, date
 import pytest
 
 from dfes.bans import TotalFireBans
-from dfes.store import file_name
+from dfes.store import file_name, InMemoryStore
 
 
 @pytest.fixture
@@ -28,3 +28,9 @@ def ban():
 
 def test_file_name(ban):
     assert file_name(ban) == "total_fire_bans_issued_2023_10_15_0408.rss"
+
+
+def test_add_to_memory_store(feed_text):
+    store = InMemoryStore()
+    store.add(feed_text)
+    assert list(store._feeds.keys())[0] == "total_fire_bans_issued_2023_01_02_0505.rss"
