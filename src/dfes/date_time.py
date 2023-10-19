684ee87430820756e0d82748282629a44c8aace1
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-19 09:06:27 +0800

    Tidy: rearrange functions.

--- src/dfes/date_time.py
+++ src/dfes/date_time.py
@@ -10,17 +10,17 @@ def extract_date(text: str) -> date:
     )
 
 
-def date_text(text: str) -> str:
-    if m := re.search(r"\d{1,2} \w+ \d{4}", text):
-        return m.group(0)
-    raise ParseException(f"Failed to find date text in {text}")
-
-
 def text_to_date(text: str) -> date:
     try:
         return datetime.strptime(text, "%d %B %Y").date()
     except ValueError:
         raise ParseException(f"Failed to parse date: {text}")
+
+
+def date_text(text: str) -> str:
+    if m := re.search(r"\d{1,2} \w+ \d{4}", text):
+        return m.group(0)
+    raise ParseException(f"Failed to find date text in {text}")
 
 
 def extract_time(text: str) -> time:
@@ -29,14 +29,14 @@ def extract_time(text: str) -> time:
     )
 
 
-def time_text(text: str) -> str:
-    if m := re.search(r"\d{2}:\d{2} (?:AM|PM)", text):
-        return m.group(0)
-    raise ParseException(f"Failed to find time text in {text}")
-
-
 def text_to_time(text: str) -> time:
     try:
         return datetime.strptime(text, "%H:%M %p").time()
     except ValueError:
         raise ParseException(f"Failed to parse time: {text}")
+
+
+def time_text(text: str) -> str:
+    if m := re.search(r"\d{2}:\d{2} (?:AM|PM)", text):
+        return m.group(0)
+    raise ParseException(f"Failed to find time text in {text}")
