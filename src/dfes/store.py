ab62e680dd5061a28df22e28a8e571119e8795eb
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-19 12:32:34 +0800

    fire ban file name generated.

--- src/dfes/store.py
+++ src/dfes/store.py
@@ -2,4 +2,5 @@ from dfes.bans import TotalFireBans
 
 
 def file_name(bans: TotalFireBans) -> str:
-    return "total_fire_bans_issued_2023_01_02_1305.rss"
+    date_formatted = bans.issued.strftime("%Y_%m_%d_%H%M")
+    return f"total_fire_bans_issued_{date_formatted}.rss"
--- tests/test_store.py
+++ tests/test_store.py
@@ -1,5 +1,24 @@
+from datetime import datetime, date
+
+import pytest
+
 from dfes import store
+from dfes.bans import TotalFireBans
 
 
-def test_file_name():
-    assert store.file_name(None) == "total_fire_bans_issued_2023_01_02_1305.rss"
+@pytest.fixture
+def ban():
+    return TotalFireBans(
+        issued=datetime.fromisoformat("2023-10-15 04:08:00+00:00"),
+        published=datetime.fromisoformat("2023-10-15 08:08:00+00:00"),
+        declared_for=date.fromisoformat("2023-10-16"),
+        locations=[
+            ('Midwest Gascoyne', 'Chapman Valley'),
+            ('Midwest Gascoyne', 'Greater Geraldton'),
+            ('Midwest Gascoyne', 'Northampton')
+        ]
+    )
+
+
+def test_file_name(ban):
+    assert store.file_name(ban) == "total_fire_bans_issued_2023_10_15_0408.rss"
