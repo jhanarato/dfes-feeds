1c61356ad0e8937eb2f48864040f420cd6cdec28
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-23 09:07:41 +0800

    Store knows not of parsers.

--- src/dfes/store.py
+++ src/dfes/store.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-from dfes.bans import total_fire_bans
-
 
 def file_name(issued: datetime) -> str:
     date_formatted = issued.strftime("%Y_%m_%d_%H%M")
@@ -12,9 +10,8 @@ class InMemoryStore:
     def __init__(self):
         self._feeds = dict()
 
-    def add(self, feed: str) -> None:
-        bans = total_fire_bans(feed)
-        self._feeds[file_name(bans.issued)] = feed
+    def add(self, issued: datetime, feed_text: str) -> None:
+        self._feeds[issued] = feed_text
 
     def retrieve(self, issued: datetime) -> str:
-        return self._feeds[file_name(issued)]
+        return self._feeds[issued]
--- tests/test_store.py
+++ tests/test_store.py
@@ -1,42 +1,14 @@
-from datetime import datetime, date
+from datetime import datetime
 
-import pytest
-
-from dfes.bans import TotalFireBans
 from dfes.store import file_name, InMemoryStore
 
 
-@pytest.fixture
-def feed_text():
-    with open("data/2023-01-03/message_TFB.rss") as file:
-        return file.read()
+def test_file_name():
+    issued = datetime.fromisoformat("2023-10-15 04:08:00+00:00")
+    assert file_name(issued) == "total_fire_bans_issued_2023_10_15_0408.rss"
 
 
-@pytest.fixture
-def ban():
-    return TotalFireBans(
-        issued=datetime.fromisoformat("2023-10-15 04:08:00+00:00"),
-        published=datetime.fromisoformat("2023-10-15 08:08:00+00:00"),
-        declared_for=date.fromisoformat("2023-10-16"),
-        locations=[
-            ('Midwest Gascoyne', 'Chapman Valley'),
-            ('Midwest Gascoyne', 'Greater Geraldton'),
-            ('Midwest Gascoyne', 'Northampton')
-        ]
-    )
-
-
-def test_file_name(ban):
-    assert file_name(ban.issued) == "total_fire_bans_issued_2023_10_15_0408.rss"
-
-
-def test_add_to_memory_store(feed_text):
+def test_retrieve_from_in_memory_store():
     store = InMemoryStore()
-    store.add(feed_text)
-    assert list(store._feeds.keys())[0] == "total_fire_bans_issued_2023_01_02_0505.rss"
-
-
-def test_retrieve_from_in_memory_store(feed_text):
-    store = InMemoryStore()
-    store.add(feed_text)
-    assert store.retrieve(datetime(2023, 1, 2, 5, 5)).startswith("<?xml version=")
+    store.add(datetime(2023, 1, 2, 5, 5), "feed test text")
+    assert store.retrieve(datetime(2023, 1, 2, 5, 5)) == "feed test text"
