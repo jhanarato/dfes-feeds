e6a378ee9dd270c466f644215bd60d39f6ebb283
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-25 09:43:57 +0800

    Tests use Jinja templates.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,58 +1,21 @@
 import pytest
 
-
-@pytest.fixture
-def xml_feed_one_entry():
-    return """
-    <?xml version="1.0" encoding="UTF-8"?>
-<rss xmlns:georss="urn:oasis:names:tc:emergency:cap:1.2"
-     xmlns:dfes="http://emergency.wa.gov.au/xmlns/dfes"
-     xmlns:dc="http://purl.org/dc/elements/1.1/"
-     xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#" version="2.0">
-    <channel>
-<title>Total Fire Ban (All Regions)</title>
-<link>http://emergency.wa.gov</link>
-<description />
-<language>en-us</language>
-<pubDate>Mon, 16 Oct 2023 08:10:56 GMT</pubDate>
-<lastBuildDate>Mon, 16 Oct 2023 08:10:56 GMT</lastBuildDate>
-<docs>http://blogs.law.harvard.edu/tech/rss</docs>
-<generator>Whispir RSS Engine</generator>
-<managingEditor>no-reply@whispir.com</managingEditor>
-<webMaster>no-reply@whispir.com</webMaster>
-<ttl>1</ttl>
-<item>
-<title>Total Fire Ban advice for 16 October 2023 </title>
-<link>https://www.emergency.wa.gov.au/#totalfirebans</link>
-<description><![CDATA[This is where the summary goes]]></description>
-<dfes:publicationTime>15/10/23 08:08 AM</dfes:publicationTime>
-<pubDate>Sun, 15 Oct 2023 08:08:08 GMT</pubDate>
-<guid>878860094</guid>
-<geo:long />
-<geo:lat />
-</item>
-</channel>
-</rss>
-    """
+from jinja2 import Environment, select_autoescape, FileSystemLoader
 
 
 @pytest.fixture
-def xml_feed_no_entry():
-    return """
-<?xml version="1.0" encoding="UTF-8"?>
-<rss xmlns:georss="urn:oasis:names:tc:emergency:cap:1.2" xmlns:dfes="http://emergency.wa.gov.au/xmlns/dfes" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#" version="2.0">
-    <channel>
-        <title>Total Fire Ban (All Regions)</title>
-        <link>http://emergency.wa.gov</link>
-        <description />
-        <language>en-us</language>
-        <pubDate>Sat, 14 Oct 2023 18:16:26 GMT</pubDate>
-        <lastBuildDate>Sat, 14 Oct 2023 18:16:26 GMT</lastBuildDate>
-        <docs>http://blogs.law.harvard.edu/tech/rss</docs>
-        <generator>Whispir RSS Engine</generator>
-        <managingEditor>no-reply@whispir.com</managingEditor>
-        <webMaster>no-reply@whispir.com</webMaster>
-        <ttl>1</ttl>
-    </channel>
-</rss>    
-    """
+def jinja_env():
+    return Environment(
+        loader=FileSystemLoader("templates/"),
+        autoescape=select_autoescape()
+    )
+
+
+@pytest.fixture
+def bans_one_entry_xml(jinja_env):
+    return jinja_env.get_template("bans_one_entry.xml").render()
+
+
+@pytest.fixture
+def bans_no_entry_xml(jinja_env):
+    return jinja_env.get_template("bans_no_entry.xml").render()
--- tests/test_feeds.py
+++ tests/test_feeds.py
@@ -5,28 +5,28 @@ import pytest
 from dfes import feeds, exceptions
 
 
-def test_no_entries(xml_feed_no_entry):
-    assert feeds.entries(xml_feed_no_entry) == []
+def test_no_entries(bans_no_entry_xml):
+    assert feeds.entries(bans_no_entry_xml) == []
 
 
-def test_one_entry(xml_feed_one_entry):
-    assert len(feeds.entries(xml_feed_one_entry)) == 1
+def test_one_entry(bans_one_entry_xml):
+    assert len(feeds.entries(bans_one_entry_xml)) == 1
 
 
-def test_get_date_published(xml_feed_one_entry):
-    entry = feeds.entries(xml_feed_one_entry)[0]
+def test_get_date_published(bans_one_entry_xml):
+    entry = feeds.entries(bans_one_entry_xml)[0]
     assert feeds.published(entry) == datetime(2023, 10, 15, 8, 8, tzinfo=timezone.utc)
 
 
-def test_published_missing(xml_feed_one_entry):
-    entry = feeds.entries(xml_feed_one_entry)[0]
+def test_published_missing(bans_one_entry_xml):
+    entry = feeds.entries(bans_one_entry_xml)[0]
     del entry['dfes_publicationtime']
     with pytest.raises(exceptions.ParseException, match="Missing RSS field: dfes_publicationtime"):
         _ = feeds.published(entry)
 
 
-def test_published_malformed(xml_feed_one_entry):
-    entry = feeds.entries(xml_feed_one_entry)[0]
+def test_published_malformed(bans_one_entry_xml):
+    entry = feeds.entries(bans_one_entry_xml)[0]
     entry['dfes_publicationtime'] = "not a timestamp"
 
     with pytest.raises(exceptions.ParseException, match="Could not parse publication time"):
