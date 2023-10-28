3b645009ebb9255dd42dc8a1be664f153c98b98c
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-28 09:39:32 +0800

    Two different feeds works. (#2)

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,5 +1,6 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, date
 
+from conftest import generate_bans_xml
 from dfes.ingest import ingest
 from dfes.repository import InMemoryRepository
 
@@ -15,3 +16,30 @@ def test_should_not_add_feed_twice(bans_xml):
     ingest(bans_xml, repo)
     ingest(bans_xml, repo)
     assert repo.list_bans() == [datetime(2023, 10, 15, 8, 8, tzinfo=timezone.utc)]
+
+
+def test_should_add_two_different_feeds():
+    regions = {"Midwest Gascoyne": ["Carnamah"]}
+
+    first = generate_bans_xml(
+        regions=regions,
+        published=datetime(2023, 10, 15, 8, 8, tzinfo=timezone.utc),
+        issued=datetime(2023, 10, 15, 17, 6, tzinfo=timezone.utc),
+        declared_for=date(2023, 10, 16),
+    )
+
+    second = generate_bans_xml(
+        regions=regions,
+        published=datetime(2023, 10, 16, 8, 8, tzinfo=timezone.utc),
+        issued=datetime(2023, 10, 15, 17, 6, tzinfo=timezone.utc),
+        declared_for=date(2023, 10, 16),
+    )
+
+    repo = InMemoryRepository()
+    ingest(first, repo)
+    ingest(second, repo)
+
+    assert repo.list_bans() == [
+        datetime(2023, 10, 15, 8, 8, tzinfo=timezone.utc),
+        datetime(2023, 10, 16, 8, 8, tzinfo=timezone.utc),
+    ]
