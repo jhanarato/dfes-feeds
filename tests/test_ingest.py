d2dd35ca47a8a87e25bdbf5b750f8ba91dc33789
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-28 09:35:04 +0800

    Test adding same feed twice.

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -4,7 +4,14 @@ from dfes.ingest import ingest
 from dfes.repository import InMemoryRepository
 
 
-def test_should_add_valid_feed_to_empty_repository(bans_xml):
+def test_should_add_feed_to_empty_repository(bans_xml):
     repo = InMemoryRepository()
+    ingest(bans_xml, repo)
+    assert repo.list_bans() == [datetime(2023, 10, 15, 8, 8, tzinfo=timezone.utc)]
+
+
+def test_should_not_add_feed_twice(bans_xml):
+    repo = InMemoryRepository()
+    ingest(bans_xml, repo)
     ingest(bans_xml, repo)
     assert repo.list_bans() == [datetime(2023, 10, 15, 8, 8, tzinfo=timezone.utc)]
