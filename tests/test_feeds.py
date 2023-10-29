c5eb48b29080b3d5d2f8847a06050d17769a5287
Ajahn Jhanarato <jhanarato@gmail.com>
2023-10-29 16:34:39 +0800

    Simplified feeds tests.

--- tests/test_feeds.py
+++ tests/test_feeds.py
@@ -11,23 +11,19 @@ def test_bozo_feed_raises_exception():
         _ = feeds.parse("Not the expected xml string")
 
 
-def test_no_entries(no_bans_xml):
+def test_parse_no_entries(no_bans_xml):
     parsed = feeds.parse(no_bans_xml)
     assert parsed.entries == []
     assert parsed.title == "Total Fire Ban (All Regions)"
     assert parsed.published == datetime(2023, 10, 14, 18, 16, 26, tzinfo=timezone.utc)
 
 
-def test_get_dfes_published(entry):
+def test_parse_entry(entry):
+    assert entry.published == datetime(2023, 10, 15, 8, 8, 8, tzinfo=timezone.utc)
     assert entry.dfes_published == datetime(2023, 10, 15, 8, 8, tzinfo=timezone.utc)
+    assert entry.summary.startswith("<div>")
 
 
 def test_dfes_published_malformed(mangled_dfes_publication):
     with pytest.raises(feeds.FeedException, match="Could not parse publication time"):
         _ = feeds.parse(mangled_dfes_publication)
-
-
-def test_construct_entry(entry):
-    assert entry.published == datetime(2023, 10, 15, 8, 8, 8, tzinfo=timezone.utc)
-    assert entry.dfes_published == datetime(2023, 10, 15, 8, 8, tzinfo=timezone.utc)
-    assert entry.summary.startswith("<div>")
