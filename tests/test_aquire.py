25f65ad1b1e02e6db6868ae10181f8b0a732fe1a
Ajahn Jhanarato <jhanarato@gmail.com>
2023-11-19 10:30:00 +0800

    Feed parsing hacked up in test.

--- tests/test_aquire.py
+++ tests/test_aquire.py
@@ -1,5 +1,10 @@
+from datetime import timezone, datetime
+
 import requests
 import responses
+
+from dfes.feeds import parse
+from dfes.urls import FIRE_BAN_URL
 
 
 def test_get_a_webpage():
@@ -11,3 +16,10 @@ def test_get_a_webpage():
 
     r = requests.get("https://bswa.org")
     assert r.text == "<html></html>"
+
+
+def test_parse_a_feed(bans_xml):
+    responses.add(responses.GET, FIRE_BAN_URL, body=bans_xml)
+    r = requests.get(FIRE_BAN_URL)
+    feed = parse(r.text)
+    assert feed.published == datetime(2023, 10, 16, 8, 10, 56, tzinfo=timezone.utc)
