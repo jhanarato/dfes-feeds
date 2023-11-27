fc520841b43f722a006896c9f7c7ea9cb4af3104
Ajahn Jhanarato <jhanarato@gmail.com>
2023-11-27 10:22:30 +0800

    Tidy.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,17 +11,15 @@ from dfes.urls import FIRE_BAN_URL
 
 def test_integration(bans_xml):
     repo = InMemoryRepository()
-
     responses.add(responses.GET, FIRE_BAN_URL, body=bans_xml)
+
     feed_text = aquire_rss_feed()
     ingest(feed_text, repo)
 
     bans_issued = repo.list_bans()[0]
     feed_text_retrieved = repo.retrieve_bans(bans_issued)
-
     parsed = parse(feed_text_retrieved)
     entry = parsed.entries[0]
-
     tfb = total_fire_bans(entry.summary)
 
     assert tfb.declared_for == date(2023, 10, 16)
