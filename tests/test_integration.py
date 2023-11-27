6c983b9913e78d83c82a1dc6b9234271a2c52985
Ajahn Jhanarato <jhanarato@gmail.com>
2023-11-27 12:43:22 +0800

    Test uses most_recent_bans()

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,7 +5,7 @@ import responses
 from dfes.bans import total_fire_bans
 from dfes.feeds import parse
 from dfes.ingest import aquire_ban_feed, ingest
-from dfes.repository import InMemoryRepository
+from dfes.repository import InMemoryRepository, most_recent_bans
 from dfes.urls import FIRE_BAN_URL
 
 
@@ -16,9 +16,8 @@ def test_integration(bans_xml):
     feed_text = aquire_ban_feed()
     ingest(feed_text, repo)
 
-    bans_issued = repo.list_bans()[0]
-    feed_text_retrieved = repo.retrieve_bans(bans_issued)
-    parsed = parse(feed_text_retrieved)
+    retrieved = most_recent_bans(repo)
+    parsed = parse(retrieved)
     entry = parsed.entries[0]
     tfb = total_fire_bans(entry.summary)
 
