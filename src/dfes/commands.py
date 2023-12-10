3780facc271049c0f55bd566216e38190642b437
Ajahn Jhanarato <jhanarato@gmail.com>
2023-12-10 10:11:52 +0800

    Extract get()

--- src/dfes/commands.py
+++ src/dfes/commands.py
@@ -8,11 +8,15 @@ from dfes.services import aquire_ban_feed, store_feed
 location = Path.home() / "dfes"
 
 
-@click.command(name="dfes")
-def dfes_command():
+def get():
     repository = FileRepository(location)
     feed = aquire_ban_feed()
     store_feed(feed, repository)
+
+
+@click.command(name="dfes")
+def dfes_command():
+    get()
 
 
 if __name__ == '__main__':
