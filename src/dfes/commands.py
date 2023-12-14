db126bb14e1a37a6f03aabf59c529cdec483b342
Ajahn Jhanarato <jhanarato@gmail.com>
2023-12-14 12:23:14 +0800

    Use command groups.

--- src/dfes/commands.py
+++ src/dfes/commands.py
@@ -4,13 +4,20 @@ from dfes.repository import FileRepository
 from dfes.services import aquire_ban_feed, store_feed, last_bans_issued, repository_location
 
 
-def get_feed():
+@click.group()
+def dfes():
+    pass
+
+
+@dfes.command(help="Retrieve and store the feed")
+def fetch():
     repository = FileRepository(repository_location())
     feed = aquire_ban_feed()
     store_feed(feed, repository)
 
 
-def show_feed():
+@dfes.command(help="Show most recently issued bans")
+def show():
     repository = FileRepository(repository_location())
     if issued := last_bans_issued(repository):
         print(f"Last bans issued: {issued.issued}")
@@ -18,15 +25,5 @@ def show_feed():
         print("No bans have been retrieved yet.")
 
 
-@click.command(name="dfes")
-@click.option("--get", "-g", is_flag=True, help="Retrieve and store the feed")
-@click.option("--show", "-s", is_flag=True, help="Show most recently issued bans")
-def dfes_command(get, show):
-    if get:
-        get_feed()
-    if show:
-        show_feed()
-
-
 if __name__ == '__main__':
-    dfes_command()
+    dfes()
