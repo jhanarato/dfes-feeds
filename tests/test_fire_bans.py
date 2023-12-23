2bdcade5ff6e0b4586f26dc2500cbda665c56c78
Ajahn Jhanarato <jhanarato@gmail.com>
2023-12-23 10:04:21 +0800

    Added a bunch of parser specifiers.

--- tests/test_fire_bans.py
+++ tests/test_fire_bans.py
@@ -14,7 +14,7 @@ def test_get_region_tags():
      <p><strong>Goldfields Midlands Region:</strong></p>
     """
 
-    tags = bans.get_region_tags(BeautifulSoup(html))
+    tags = bans.get_region_tags(BeautifulSoup(html, features="html.parser"))
     strings = [tag.string for tag in tags]
     assert strings == ["Midwest Gascoyne Region:",
                        "Perth Metropolitan Region:",
@@ -27,7 +27,7 @@ def test_missing_region_tags():
     <p><strong>Not what you're looking for</strong></p>
     """
 
-    assert bans.get_region_tags(BeautifulSoup(html)) == []
+    assert bans.get_region_tags(BeautifulSoup(html, features="html.parser")) == []
 
 
 def test_get_district_tags_from_region_tag():
@@ -40,7 +40,7 @@ def test_get_district_tags_from_region_tag():
     <li>Coorow - All Day</li> 
     </ul>
     """
-    region_tag = bans.get_region_tags(BeautifulSoup(html))[0]
+    region_tag = bans.get_region_tags(BeautifulSoup(html, features="html.parser"))[0]
     tags = bans.get_district_tags(region_tag)
 
     strings = [tag.string for tag in tags]
@@ -51,20 +51,21 @@ def test_get_district_tags_from_region_tag():
 
 def test_date_of_issue():
     html = "<span style=\"color: #777777;\"> Date of issue: 02 January 2023 </span>"
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, features="html.parser")
     assert bans.date_of_issue(soup) == date(2023, 1, 2)
 
 
 def test_find_tag_contents_ok():
     soup = BeautifulSoup(
-        "<span style=\"color: #777777;\"> Date of issue: 02 January 2023 </span>"
+        "<span style=\"color: #777777;\"> Date of issue: 02 January 2023 </span>",
+        features = "html.parser"
     )
     contents = bans.find_tag_contents(soup, "span", "Date of issue:")
     assert contents == "Date of issue: 02 January 2023"
 
 
 def test_find_tag_contents_with_missing_tag():
-    soup = BeautifulSoup("<p>not a span</p>")
+    soup = BeautifulSoup("<p>not a span</p>", features="html.parser")
     with pytest.raises(ParsingFailed,
                        match="No <span> tag found"):
         _ = bans.find_tag_contents(soup, "span", "Date of issue:")
@@ -73,8 +74,8 @@ def test_find_tag_contents_with_missing_tag():
 @pytest.mark.parametrize(
     "soup,looking_for,result",
     [
-        (BeautifulSoup("<p>contents</p>"), "contents", True),
-        (BeautifulSoup("<p>contents</p>"), "mismatch", False),
+        (BeautifulSoup("<p>contents</p>", features="html.parser"), "contents", True),
+        (BeautifulSoup("<p>contents</p>", features="html.parser"), "mismatch", False),
     ]
 
 )
@@ -84,7 +85,8 @@ def test_tag_exists_containing(soup, looking_for, result):
 
 def test_time_of_issue():
     soup = BeautifulSoup(
-        "<span style=\"color: #777777;\">Time of issue: 05:05 PM </span>"
+        "<span style=\"color: #777777;\">Time of issue: 05:05 PM </span>",
+        features="html.parser"
     )
     assert bans.time_of_issue(soup) == time(17, 5)
 
@@ -92,7 +94,7 @@ def test_time_of_issue():
 def test_date_delclared_for():
     soup = BeautifulSoup("""
     <p>A Total Fire Ban has been declared for 3 January 2023 for the local government districts listed below:</p>
-    """)
+    """, features="html.parser")
 
     assert bans.date_declared_for(soup) == date(2023, 1, 3)
 
@@ -100,7 +102,7 @@ def test_date_delclared_for():
 def test_date_revoked_for():
     soup = BeautifulSoup("""
     <p>The Total Fire Ban declared for 10 December 2023 has been revoked for the local government districts listed below:</p>
-    """)
+    """, features="html.parser")
 
     assert bans.date_revoked_for(soup) == date(2023, 12, 10)
 
@@ -113,7 +115,7 @@ def test_date_revoked_for():
     ]
 )
 def test_bans_are_revoked(tag_contents, revoked):
-    assert bans.bans_are_revoked(BeautifulSoup(tag_contents)) == revoked
+    assert bans.bans_are_revoked(BeautifulSoup(tag_contents, features="html.parser")) == revoked
 
 
 def test_locations():
@@ -129,7 +131,7 @@ def test_locations():
     <ul>
     <li>Armadale - All Day</li>
     </ul>
-    """)
+    """, features="html.parser")
     assert list(bans.locations(soup)) == [
         ("Midwest Gascoyne", "Carnamah"),
         ("Midwest Gascoyne", "Chapman Valley"),
@@ -139,12 +141,12 @@ def test_locations():
 
 
 def test_extract_district():
-    tag = BeautifulSoup("<li>Bunbury - All Day</li>").find('li')
+    tag = BeautifulSoup("<li>Bunbury - All Day</li>", features="html.parser").find('li')
     assert bans.extract_district(tag) == "Bunbury"
 
 
 def test_extract_region():
-    tag = BeautifulSoup("<strong>Midwest Gascoyne Region:</strong>").find('strong')
+    tag = BeautifulSoup("<strong>Midwest Gascoyne Region:</strong>", features="html.parser").find('strong')
     assert bans.extract_region(tag) == "Midwest Gascoyne"
 
 
