e80f8bfa5ae7e33077c80e6ecb5da6043f542b42
Ajahn Jhanarato <jhanarato@gmail.com>
2024-01-15 10:36:24 +0800

    New fixture using dataclasses.

--- tests/test_analyse.py
+++ tests/test_analyse.py
@@ -1,9 +1,71 @@
-from datetime import datetime
+from datetime import datetime, date
 
 import polars as pl
 import pytest
 
-from dfes.analyze import extra_entries, with_n_extras, filter_extras
+from dfes.analyze import extra_entries, with_n_extras, filter_extras, to_dataframe
+from dfes.bans import TotalFireBans
+from dfes.feeds import Feed, Entry
+
+
+@pytest.fixture
+def feeds():
+    return to_dataframe(
+        [
+            Feed(
+                title="Total Fire Ban (All Regions)",
+                published=datetime(2000, 1, 1, 1),
+                entries=[
+                    Entry(
+                        published=datetime(2000, 1, 1, 1, 1),
+                        dfes_published=datetime(2000, 1, 1, 1, 1, 1),
+                        summary="",
+                        bans=TotalFireBans(
+                            revoked=False,
+                            issued=datetime(2000, 1, 1, 1, 1, 1, 1),
+                            declared_for=date(2000, 1, 2),
+                            locations=[("Armadale", "Perth Metropolitan")]
+                        )
+                    ),
+                    Entry(
+                        published=datetime(2000, 1, 2, 1, 1),
+                        dfes_published=datetime(2000, 1, 2, 1, 1, 1),
+                        summary="",
+                        bans=TotalFireBans(
+                            revoked=False,
+                            issued=datetime(2000, 1, 2, 1, 1, 1, 1),
+                            declared_for=date(2000, 1, 3),
+                            locations=[
+                                ("Goldfields Midlands", "Toodyay"),
+                                ("South West", "Murray"),
+                            ]
+                        )
+                    ),
+                ]
+            ),
+            Feed(
+                title="Total Fire Ban (All Regions)",
+                published=datetime(2000, 1, 3, 1),
+                entries=[
+                    Entry(
+                        published=datetime(2000, 1, 3, 1, 1),
+                        dfes_published=datetime(2000, 1, 3, 1, 1, 1),
+                        summary="",
+                        bans=TotalFireBans(
+                            revoked=False,
+                            issued=datetime(2000, 1, 3, 1, 1, 1, 1),
+                            declared_for=date(2000, 1, 4),
+                            locations=[
+                                ("Armadale", "Perth Metropolitan"),
+                                ("Goldfields Midlands", "Toodyay"),
+                                ("South West", "Murray"),
+                            ]
+                        )
+                    )
+                ]
+            )
+        ]
+    )
 
 
 @pytest.fixture
