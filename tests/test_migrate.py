1ec21f5e2f37bf8cc593d77f8466ca58cc05d8b7
Ajahn Jhanarato <jhanarato@gmail.com>
2024-01-18 12:39:24 +0800

    Added tests.

--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -1,17 +1,28 @@
 from datetime import datetime, timezone
+from pathlib import Path
 
 from conftest import generate_bans_xml
 from dfes.migrate import migrate_to_seconds, missing_seconds
 from dfes.repository import FileRepository
 
 
+def write_without_seconds(feed_published: datetime, repository_path: Path):
+    feed_text = generate_bans_xml(feed_published=feed_published)
+    file_path = repository_path / feed_published.strftime("bans_issued_%Y_%m_%d_%H%M.rss")
+    file_path.write_text(feed_text)
+
+
+def write_with_seconds(feed_published: datetime, repository_path: Path):
+    feed_text = generate_bans_xml(feed_published=feed_published)
+    file_path = repository_path / feed_published.strftime("bans_issued_%Y_%m_%d_%H%M%S.rss")
+    file_path.write_text(feed_text)
+
+
 def test_migrate_to_seconds(tmp_path):
     repository = FileRepository(tmp_path)
 
     feed_published = datetime(2021, 1, 1, 1, 1, 17, tzinfo=timezone.utc)
-    feed_text = generate_bans_xml(feed_published=feed_published)
-    file_path = tmp_path / feed_published.strftime("bans_issued_%Y_%m_%d_%H%M.rss")
-    file_path.write_text(feed_text)
+    write_without_seconds(feed_published, tmp_path)
 
     assert repository.list_bans() == []
 
@@ -42,6 +53,17 @@ def test_should_migrate_empty_repository(tmp_path):
     migrate_to_seconds(repository)
 
 
-def test_should_migrate_when_all_have_seconds(tmp_path):
+def test_should_migrate_when_none_have_seconds(tmp_path):
     repository = FileRepository(tmp_path)
+
+    feeds_published = [
+        datetime(2021, 1, 1, 12, 1, tzinfo=timezone.utc),
+        datetime(2021, 1, 1, 12, 2, tzinfo=timezone.utc),
+    ]
+
+    for feed_published in feeds_published:
+        write_without_seconds(feed_published, tmp_path)
+
     migrate_to_seconds(repository)
+
+    assert repository.list_bans() == feeds_published
