9a3af5ceccc8922a1789f05393025c63cf52ed25
Ajahn Jhanarato <jhanarato@gmail.com>
2024-02-08 12:09:38 +0800

    Reworked test to use new data generator.

--- tests/test_show.py
+++ tests/test_show.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone, date
 
-from conftest import generate_bans_xml
+from conftest import generate_bans_xml, generate_with_no_entries
 from dfes.fetch import store_feed
 from dfes.show import most_recently_issued
 
@@ -24,16 +24,17 @@ def test_should_be_none_when_repository_empty(repository):
     assert most_recently_issued(repository) is None
 
 
-def test_should_ignore_feeds_with_no_entries(repository, no_bans_xml):
-    store_feed(no_bans_xml, repository)
-
-    assert datetime(2023, 10, 14, 18, 16, 26, tzinfo=timezone.utc) in repository.list_bans()
-
+def test_should_ignore_feeds_with_no_entries(repository):
     earlier_feed = generate_bans_xml(
-        feed_published=datetime(2023, 10, 12, tzinfo=timezone.utc),
+        feed_published=datetime(2023, 10, 12),
         declared_for=date(2023, 10, 13)
     )
 
+    later_feed = generate_with_no_entries(
+        feed_published=datetime(2023, 10, 13)
+    )
+
     store_feed(earlier_feed, repository)
+    store_feed(later_feed, repository)
 
     assert most_recently_issued(repository).declared_for == date(2023, 10, 13)
