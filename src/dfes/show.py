7286c57849835644ad7313eda2a6e6337407e665
Ajahn Jhanarato <jhanarato@gmail.com>
2024-02-08 17:07:38 +0800

    Extract method.

--- src/dfes/show.py
+++ src/dfes/show.py
@@ -10,11 +10,15 @@ def most_recently_issued(repository: Repository) -> TotalFireBans | None:
         feed = parse_feed(feed_text)
 
         if feed.entries:
-            for entry in feed.entries:
-                entry.parse_summary()
+            parse_all_summaries(feed)
             return last_issued(feed).bans
 
     return None
+
+
+def parse_all_summaries(feed: Feed):
+    for entry in feed.entries:
+        entry.parse_summary()
 
 
 def last_issued(feed: Feed) -> Entry:
