dff97667a47a45472be517106a8312e6997724dc
Ajahn Jhanarato <jhanarato@gmail.com>
2024-02-11 10:24:12 +0800

    Display both declared and revoked.

--- src/dfes/reports.py
+++ src/dfes/reports.py
@@ -5,7 +5,10 @@ def display_bans(latest_bans: tuple[TotalFireBans, ...]) -> None:
     for bans in latest_bans:
         print(f"Total Fire Bans")
         print(f"Issued: {bans.issued}")
-        print(f"Declared for: {bans.declared_for}")
-        print("")
+        if bans.revoked:
+            print(f"Revoked for: {bans.declared_for}")
+        else:
+            print(f"Declared for: {bans.declared_for}")
         for location in bans.locations:
             print(f"{location[0]} / {location[1]}")
+        print("")
