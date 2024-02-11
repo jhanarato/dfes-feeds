9483ef0dddae081eb3a64e4acbbbc6b671ed115a
Ajahn Jhanarato <jhanarato@gmail.com>
2024-02-11 08:47:56 +0800

    Group tests in class.

--- tests/test_show.py
+++ tests/test_show.py
@@ -9,42 +9,41 @@ from dfes.fetch import store_feed
 from dfes.show import most_recently_issued, last_issued, parse_feeds, bans_to_show, LatestInFeed
 
 
-def test_should_be_none_when_repository_empty(repository):
-    assert most_recently_issued(repository) is None
+class TestMostRecentlyIssued:
+    def test_should_be_none_when_repository_empty(self, repository):
+        assert most_recently_issued(repository) is None
 
+    def test_should_get_most_recently_issued(self, repository):
+        earlier_feed = generate_bans_xml(
+            feed_published=datetime(2023, 10, 12),
+            issued=datetime(2023, 10, 12),
+        )
 
-def test_should_get_most_recently_issued(repository):
-    earlier_feed = generate_bans_xml(
-        feed_published=datetime(2023, 10, 12),
-        issued=datetime(2023, 10, 12),
-    )
+        later_feed = generate_bans_xml(
+            feed_published=datetime(2023, 10, 13),
+            issued=datetime(2023, 10, 13),
+        )
 
-    later_feed = generate_bans_xml(
-        feed_published=datetime(2023, 10, 13),
-        issued=datetime(2023, 10, 13),
-    )
+        store_feed(earlier_feed, repository)
+        store_feed(later_feed, repository)
 
-    store_feed(earlier_feed, repository)
-    store_feed(later_feed, repository)
+        bans = most_recently_issued(repository)
+        assert bans.issued == datetime(2023, 10, 13, tzinfo=timezone.utc)
 
-    bans = most_recently_issued(repository)
-    assert bans.issued == datetime(2023, 10, 13, tzinfo=timezone.utc)
+    def test_should_ignore_feeds_with_no_entries(self, repository):
+        earlier_feed = generate_bans_xml(
+            feed_published=datetime(2023, 10, 12),
+            declared_for=date(2023, 10, 13)
+        )
 
+        later_feed = generate_with_no_entries(
+            feed_published=datetime(2023, 10, 13)
+        )
 
-def test_should_ignore_feeds_with_no_entries(repository):
-    earlier_feed = generate_bans_xml(
-        feed_published=datetime(2023, 10, 12),
-        declared_for=date(2023, 10, 13)
-    )
+        store_feed(earlier_feed, repository)
+        store_feed(later_feed, repository)
 
-    later_feed = generate_with_no_entries(
-        feed_published=datetime(2023, 10, 13)
-    )
-
-    store_feed(earlier_feed, repository)
-    store_feed(later_feed, repository)
-
-    assert most_recently_issued(repository).declared_for == date(2023, 10, 13)
+        assert most_recently_issued(repository).declared_for == date(2023, 10, 13)
 
 
 @pytest.fixture
