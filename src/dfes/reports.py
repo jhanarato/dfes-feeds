27dfe21cf6a3887d24b2d72a96105c7d3ad4cee7
Ajahn Jhanarato <jhanarato@gmail.com>
2024-02-20 10:23:30 +0800

    Refactor: extract print_bans

--- src/dfes/reports.py
+++ src/dfes/reports.py
@@ -1,14 +1,20 @@
+from rich import print
+
 from dfes.bans import TotalFireBans
 
 
 def display_bans(latest_bans: tuple[TotalFireBans, ...]) -> None:
     for bans in latest_bans:
-        print(f"Total Fire Bans")
-        print(f"Issued: {bans.issued}")
-        if bans.revoked:
-            print(f"Revoked for: {bans.declared_for}")
-        else:
-            print(f"Declared for: {bans.declared_for}")
-        for location in bans.locations:
-            print(f"{location[0]} / {location[1]}")
-        print("")
+        print_ban(bans)
+
+
+def print_ban(bans: TotalFireBans):
+    print(f"Total Fire Bans")
+    print(f"Issued: {bans.issued}")
+    if bans.revoked:
+        print(f"Revoked for: {bans.declared_for}")
+    else:
+        print(f"Declared for: {bans.declared_for}")
+    for location in bans.locations:
+        print(f"{location[0]} / {location[1]}")
+    print("")
