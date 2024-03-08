3dd5e9040fac2d7f7a6d6f29f48a2e06c18972c8
Ajahn Jhanarato <jhanarato@gmail.com>
2024-03-08 12:53:18 +0800

    Extract jinja_env() function.

--- tests/generate.py
+++ tests/generate.py
@@ -9,12 +9,14 @@ def dfes_published(value: datetime):
     return value.strftime("%d/%m/%y %H:%M %p")
 
 
-def generate_feed(feed: Feed) -> str:
-    env = jinja2.Environment(
+def jinja_env() -> jinja2.Environment:
+    return jinja2.Environment(
         loader=jinja2.FileSystemLoader("templates/"),
         autoescape=jinja2.select_autoescape(),
         trim_blocks=True,
         lstrip_blocks=True,
     )
 
-    return env.get_template("new_bans.xml").render(feed=feed)
+
+def generate_feed(feed: Feed) -> str:
+    return jinja_env().get_template("new_bans.xml").render(feed=feed)
