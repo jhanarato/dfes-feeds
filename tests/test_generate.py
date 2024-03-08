a8b0d5bb031e3eb1119cd0e896fecec4ddbdd3a2
Ajahn Jhanarato <jhanarato@gmail.com>
2024-03-08 10:57:56 +0800

    RSS generator adds entry.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,16 +1,34 @@
 from datetime import datetime, timezone
 
-from dfes.feeds import Feed, parse_feed
+from dfes.feeds import Feed, parse_feed, Entry
 from generate import generate_feed
 
 
 class TestGenerateFeed:
-    def test_generates_feed_published(self):
-        published = datetime(2000, 1, 1, 1, tzinfo=timezone.utc)
+    def test_generate_feed_with_no_entries(self):
         feed_in = Feed(
             title="Total Fire Ban (All Regions)",
-            published=published,
+            published=datetime(2000, 1, 1, 1, tzinfo=timezone.utc),
             entries=[],
+        )
+
+        feed_text = generate_feed(feed_in)
+        feed_out = parse_feed(feed_text)
+
+        assert feed_out == feed_in
+
+    def test_feed_with_entry(self):
+        feed_in = Feed(
+            title="Total Fire Ban (All Regions)",
+            published=datetime(2000, 1, 1, 1, tzinfo=timezone.utc),
+            entries=[
+                Entry(
+                    published=datetime(2000, 1, 1, 2, tzinfo=timezone.utc),
+                    dfes_published=datetime(2000, 1, 1, 2, tzinfo=timezone.utc),
+                    summary="A summary",
+                    bans=None,
+                ),
+            ],
         )
 
         feed_text = generate_feed(feed_in)
