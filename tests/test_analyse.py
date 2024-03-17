474aa08b832054b1d73d5e68f88cd5d2be857a5c
Ajahn Jhanarato <jhanarato@gmail.com>
2024-03-17 16:28:43 +0800

    Fixture is unused. Removed.

--- tests/test_analyse.py
+++ tests/test_analyse.py
@@ -4,70 +4,8 @@ from zoneinfo import ZoneInfo
 import polars as pl
 import pytest
 
-from dfes.analyze import to_dataframe, n_entries, issued_to_declared, format_datetime, col_interval_minutes, \
+from dfes.analyze import n_entries, issued_to_declared, format_datetime, col_interval_minutes, \
     datetime_to_hour, perth_tz
-from dfes.feeds import Item, Feed
-from dfes.model import TotalFireBans, AffectedAreas
-
-
-@pytest.fixture
-def feeds_df():
-    return to_dataframe(
-        [
-            Feed(
-                title="Total Fire Ban (All Regions)",
-                published=datetime(2000, 1, 1),
-                items=[
-                    Item(
-                        published=datetime(2000, 1, 1, 1),
-                        dfes_published=datetime(2000, 1, 1, 2),
-                        description="",
-                        bans=TotalFireBans(
-                            revoked=False,
-                            issued=datetime(2000, 1, 1, 3),
-                            declared_for=date(2000, 1, 2),
-                            locations=AffectedAreas([("Armadale", "Perth Metropolitan")])
-                        )
-                    ),
-                    Item(
-                        published=datetime(2000, 1, 2, 1),
-                        dfes_published=datetime(2000, 1, 2, 2),
-                        description="",
-                        bans=TotalFireBans(
-                            revoked=False,
-                            issued=datetime(2000, 1, 2, 3),
-                            declared_for=date(2000, 1, 3),
-                            locations=AffectedAreas([
-                                ("Goldfields Midlands", "Toodyay"),
-                                ("South West", "Murray"),
-                            ])
-                        )
-                    ),
-                ]
-            ),
-            Feed(
-                title="Total Fire Ban (All Regions)",
-                published=datetime(2000, 1, 3, 1),
-                items=[
-                    Item(
-                        published=datetime(2000, 1, 3, 1),
-                        dfes_published=datetime(2000, 1, 3, 2),
-                        description="",
-                        bans=TotalFireBans(
-                            revoked=False,
-                            issued=datetime(2000, 1, 3, 3),
-                            declared_for=date(2000, 1, 4),
-                            locations=[
-                                ("Armadale", "Perth Metropolitan"),
-                                ("Goldfields Midlands", "Toodyay"),
-                                ("South West", "Murray"),
-                            ]
-                        )
-                    )
-                ]
-            )
-        ]
-    )
 
 
 def test_issued_to_declared():
