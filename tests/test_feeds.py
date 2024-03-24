d00c004b5f816afd8f51b194e35165e48dde0d9b
Ajahn Jhanarato <jhanarato@gmail.com>
2024-03-24 12:18:16 +0800

    Test deleted.

--- tests/test_feeds.py
+++ tests/test_feeds.py
@@ -1,11 +1,9 @@
 from datetime import datetime, timezone
-from zoneinfo import ZoneInfo
 
 import pytest
 
-from conftest import generate_bans_xml
 from dfes.exceptions import ParsingFailed
-from dfes.feeds import Item, parse_feed, parse_feeds, Feed
+from dfes.feeds import parse_feed, Feed
 from generate import render_feed_as_rss, create_feed
 
 
@@ -37,31 +35,3 @@ def test_parse_item():
     parsed = parse_feed(rss)
     assert parsed.items[0].published == datetime(2000, 1, 1, 0, 0, tzinfo=timezone.utc)
     assert parsed.items[0].description.startswith("<div>")
-
-
-def test_parse_item_description():
-    issued = datetime(2000, 1, 2, 3, 4, tzinfo=ZoneInfo(key='Australia/Perth'))
-    feed_xml = generate_bans_xml(issued=issued)
-
-    description = parse_feed(feed_xml).items[0].description
-
-    item = Item(
-        published=datetime(2001, 1, 1),
-        description=description,
-    )
-
-    item.parse_description()
-
-    assert item.bans.issued == issued
-
-
-def test_parse_feeds():
-    feed_published = datetime(2000, 1, 1, 0, 0, tzinfo=timezone.utc)
-
-    feeds_text = [
-        generate_bans_xml(feed_published=feed_published),
-        generate_bans_xml(feed_published=feed_published),
-    ]
-
-    for feed in parse_feeds(feeds_text):
-        assert feed.published == feed_published
